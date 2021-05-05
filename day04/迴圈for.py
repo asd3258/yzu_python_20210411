a356d8c04afc61add604d8b7b611652c72f46d59
Lesson10 <Neil10241126@gmail.com>
2021-05-05 21:13:20 +0800

    ss

---
+++ day04/迴圈for.py
@@ -0,0 +1,10 @@
+# for-loop (步進)
+for i in range(1, 5):
+    print(i, 'java')
+for i in range(1, 10, 2):
+    print(i, 'java')
+
+# for-in (尋訪)
+for score in [100, 90, 50, 70]:
+    if score >= 60:
+        print(score)
