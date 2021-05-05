a7bda4df427666d4fe618268ed5e90c396618200
Lesson10 <Neil10241126@gmail.com>
2021-05-05 20:57:33 +0800

    ss

--- day04/猜數字對戰2.py
+++ day04/猜數字對戰2.py
@@ -2,8 +2,20 @@ import random as r
 ans = 47
 min = 0
 max = 100
-
 count = 50
+
+# 判定結果
+def check(guess, who):
+    global min, max
+    if guess > ans:
+        max = guess
+    elif guess < ans:
+        min = guess
+    else:
+        print('%s答對了' % who )
+        return True
+    return False
+
 while count > 0:
     # 使用者猜
     guess = int(input('(%d). 請輸入 %d ~ %d : ' % (count, min, max)))
@@ -11,24 +23,13 @@ while count > 0:
     if guess >= max or guess <= min:
         print('數字範圍錯誤')
         continue
-
-    if guess > ans:
-        max = guess
-    elif guess < ans:
-        min = guess
-    else:
-        print('使用者答對了')
+    if check(guess, '使用者'):
         break
 
     # 電腦猜
     guess = r.randint(min+1, max-1)
     print('(%d). 電腦猜 %d ~ %d : %d ' % (count, min, max, guess))
-    if guess > ans:
-        max = guess
-    elif guess < ans:
-        min = guess
-    else:
-        print('電腦答對了')
+    if check(guess, '電腦'):
         break
 
     count = count - 1
